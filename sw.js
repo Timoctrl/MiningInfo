7c5d5c41d10ced586a37d4e666371668213da4b4
Timoctrl <timostle@gmail.com>
2026-03-30 15:00:41 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE = "nerdminer-v1";
+const ASSETS = ["/", "/index.html", "/manifest.json", "/icon.png"];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(()=>{}));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", e => {
+  const url = new URL(e.request.url);
+  // Don't cache API calls to pool or local miners
+  if(url.hostname !== location.hostname) return;
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
